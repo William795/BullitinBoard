130dad547462b66e7bda8da78979e8749c7e665c
William Moody <williammoody@Williams-MacBook-Air.local>
2019-06-03 10:37:06 -0600

    first commit

---
+++ BullitinBoard/BullitinBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullitinBoard
+//
+//  Created by William Moody on 6/3/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
